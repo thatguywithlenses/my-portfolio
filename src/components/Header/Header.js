059822c0bd4cf5bcd12b4ad21d4581e5ec9d3fec
Punam Chand Guru <punamchandguru@gmail.com>
2022-06-01 19:52:37 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,8 +10,7 @@ const Header = () => {
         <span>Hi, my name is</span>
         <div className='para1'>Punam Chand Guru.</div>
         <div className='para2'>I build things for the web.</div>
-        <div className='para3'>I am Frontend developer currently finding opportunity as Front End Web
-            Developer in a highly technical company
+        <div className='para3'>I am Frontend developer currently finding opportunity in a highly technical company
             where I could utilize my skills in Web Design, 
             Front-End Web Development.</div>
             <a href={Resume} download className='btn'>Resume</a>
