79ade098dcef8dd4faf51628aa55b54e715014a6
Punam Chand Guru <punamchandguru@gmail.com>
2022-06-05 15:08:01 +0530

    Update SideComp.js

--- src/components/SideComp.js
+++ src/components/SideComp.js
@@ -10,7 +10,7 @@ const SideComp = () => {
           <li >
             <a href='https://github.com/thatguywithlenses' ><FiGithub size={20} /></a></li>
           <li >
-            <a href='https://www.instagram.com/air_particle/'><FiInstagram size={20} /></a></li>
+            <a href='https://www.instagram.com/thatguywithlenses/'><FiInstagram size={20} /></a></li>
           <li ><a href='https://twitter.com/pawanguru011'><FiTwitter size={20} /></a></li>
           <li ><a href='https://www.linkedin.com/in/punam-chand-guru-8836a5194'><FiLinkedin size={20} /></a></li>
       </ul>
